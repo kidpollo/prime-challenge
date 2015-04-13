d3a873b0436c86a5a0aa69a1946e958c238193ef
@kidpollo <kidpollo@gmail.com>
2015-04-13 13:51:53 -0700

    add tests and explain things a bit more

--- src/prime_challenge/core.clj
+++ src/prime_challenge/core.clj
@@ -3,14 +3,16 @@
 
 (def known-primes #{2, 3, 5, 7, 11, 13, 17, 19, 23, 29})
 
-(defn divisible-in? [n coll]
+(defn divisible-in?
+  "returns true if any of the numbers in the collection are divisible by n"
+  [n coll]
   (boolean (some #(= 0 (rem n %)) coll)))
 
-(defn test-primality [n]
-  (if (or (divisible-in? n known-primes)
-          (divisible-in? n (range 2 n)))
-    false
-    true))
+(defn test-primality
+  "returns true if n is divisible by one of the known primes or is not divisible by any m from 2 to n - 1"
+  [n known-primes]
+  (not (or (divisible-in? n known-primes)
+           (divisible-in? n (range 2 n)))))
 
 (defn- primes-between
   "Finds prime numbers within a range"
@@ -32,6 +34,6 @@
     (do
       ; async add primes to known primes maybe?
       #_(async/thread (primes-between (+ 1 (max known-primes)) n))
-      (test-primality n))
+      (test-primality n known-primes))
 
     (boolean (some #{n} known-primes))))
--- test/prime_challenge/core_test.clj
+++ test/prime_challenge/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [prime-challenge.core :refer :all]))
 
+(deftest divisible-in?-test
+  (is (= true (divisible-in? 2 #{1 2})))
+  (is (= true (divisible-in? 2 #{1})))
+  (is (= false (divisible-in? 5 #{2 3 4})))
+  (is (= true (divisible-in? 30 #{2 3 4}))))
+
+(deftest test-primality-test
+  (is (= true (test-primality 2 #{})))
+  (is (= false (test-primality 2 #{2})))
+  (is (= true (test-primality 29 #{})))
+  (is (= false (test-primality 30 #{})))
+  (is (= true (test-primality 31 #{}))))
+
 (deftest prime?-test
   (testing "A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself."
     (is (= false (prime? -1)))
